d9b7970f9de39a18ef2407e1fb291b044c086f6f
zqleow13 <hayleyleow13@gmail.com>
2023-09-05 20:56:03 +0800

    Starting new html, css and js files

---
+++ Wordle Clone/script.js
@@ -0,0 +1,43 @@
+function checkFilled1() {
+    var inputVal = document.getElementById("sq1");
+    if (inputVal.value == "L") {
+        inputVal.style.backgroundColor = "green";
+    }
+    else{
+        inputVal.style.backgroundColor = "red";
+    }
+}
+
+function checkFilled2() {
+    var inputVal = document.getElementById("sq2");
+    if (inputVal.value == "O") {
+        inputVal.style.backgroundColor = "green";
+    }
+    else{
+        inputVal.style.backgroundColor = "red";
+    }
+}
+    var inputVal = document.getElementById("sq3");
+    if (inputVal.value == "V") {
+        inputVal.style.backgroundColor = "green";
+    }
+    else{
+        inputVal.style.backgroundColor = "red";
+    }
+    var inputVal = document.getElementById("sq4");
+    if (inputVal.value == "E") {
+        inputVal.style.backgroundColor = "green";
+    }
+    else{
+        inputVal.style.backgroundColor = "red";
+    }
+    var inputVal = document.getElementById("sq5");
+    if (inputVal.value == "R") {
+        inputVal.style.backgroundColor = "green";
+    }
+    else{
+        inputVal.style.backgroundColor = "red";
+    }
+
+
+checkFilled();
