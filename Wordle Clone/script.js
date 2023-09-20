8e18b6b92b230c0233d7e15e662e1be1f3151583
zqleow13 <hayleyleow13@gmail.com>
2023-09-20 15:28:08 +0800

    Did the grid with JS!

--- Wordle Clone/script.js
+++ Wordle Clone/script.js
@@ -1,1 +1,33 @@
 let grid = document.querySelector('#game');
+
+let guessesAllowed = 6;
+
+let wordLength = 5;
+
+
+// generate 5 rows
+let fragment = document.createDocumentFragment();
+
+let generateGrid = () => {
+
+Array.from({length: guessesAllowed}).forEach(() => {
+    let row = document.createElement('div');
+    row.classList.add('row');
+
+Array.from({length: wordLength}).forEach(() => {
+    let tile = document.createElement('div');
+    tile.classList.add('tile');
+
+    row.appendChild(tile);
+});
+
+    fragment.appendChild(row);
+});
+
+grid.appendChild(fragment);
+
+};
+
+// where each row contains 6 columns
+
+// append to grid (not sure what this means tbh)
