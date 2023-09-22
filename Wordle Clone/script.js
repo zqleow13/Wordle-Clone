630cdc6c30a0db7858b4c6b5ea1c5c279d978e85
zqleow13 <hayleyleow13@gmail.com>
2023-09-22 16:53:47 +0800

    solved the JS bug! added in color func for letters

--- Wordle Clone/script.js
+++ Wordle Clone/script.js
@@ -1,4 +1,4 @@
-let grid = document.querySelector('#game');
+/*let grid = document.querySelector('#game');
 
 let guessesAllowed = 6;
 
@@ -31,16 +31,16 @@ grid.appendChild(fragment);
 
 // append to grid (not sure what this means tbh)
 
-// when user press button, the button works and the letter appear
+// when user press button, the button works and the letter appear */
 
-const buttonElements = document.querySelectorAll("button");
+const buttonElements = document.querySelectorAll('button');
 let row = 1;
 let letter = 1;
-const wordForTheDay = 'Queue';
+const wordForTheDay = "Queue";
 const wordElements = document.querySelectorAll(".row");
 
 buttonElements.forEach((element) => {
-    element.addEventListener("click", function() {
+    element.addEventListener('click', function() {
         keypress(element.attributes["data-key"].value)
     });
 });
@@ -53,9 +53,37 @@ function populateWord(key) {
     }
 }
 
+function checkWord() {
+    const letterElements = wordElements[row - 1].querySelectorAll(".tile");
+
+    letterElements.forEach((element, index) => {
+        const indexOfLetterInWordOfTheDay = wordForTheDay.toLowerCase().indexOf
+        (element.innerText.toLowerCase());
+
+        if (indexOfLetterInWordOfTheDay === index) {
+            element.classList.add('word-green');
+        } else if (indexOfLetterInWordOfTheDay > 0) {
+            element.classList.add('word-yellow');
+        } else {
+            element.classList.add('word-grey'); 
+        }
+    });
+}
+
+
+
+
+function enterWord() {
+    if (letter < 6) {
+        alert('Not enough letters');
+    } else {
+        checkWord();
+    }
+}
+
 function keypress(key) {
     if (key.toLowerCase() === 'enter') {
-        // enterWord();
+        enterWord();
     } else if (key.toLowerCase() === 'del') {
         // deleteLetter();
     } else {
