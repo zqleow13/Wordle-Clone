492dfd1739b392bce884f0b1947aea53a0f57004
Leow Zhi Qing (Hayley) <142128275+zqleow13@users.noreply.github.com>
2023-09-26 13:36:15 +0800

    Removed all comments from script.js

--- Wordle Clone/script.js
+++ Wordle Clone/script.js
@@ -1,38 +1,3 @@
-/*let grid = document.querySelector('#game');
-
-let guessesAllowed = 6;
-
-let wordLength = 5;
-
-// generate 5 rows
-let fragment = document.createDocumentFragment();
-
-let generateGrid = () => {
-
-Array.from({length: guessesAllowed}).forEach(() => {
-    let row = document.createElement('div');
-    row.classList.add('row');
-
-Array.from({length: wordLength}).forEach(() => {
-    let tile = document.createElement('div');
-    tile.classList.add('tile');
-
-    row.appendChild(tile);
-});
-
-    fragment.appendChild(row);
-});
-
-grid.appendChild(fragment);
-
-};
-
-// where each row contains 6 columns
-
-// append to grid (not sure what this means tbh)
-
-// when user press button, the button works and the letter appear */
-
 const buttonElements = document.querySelectorAll('button');
 let row = 1;
 let letter = 1;
